032b9d7513eed195a661dbd17fc374db675d974d
Oleg <obatrov@yandex.ru>
2020-10-28 07:16:08 +0000

    Added the entire project

---
+++ ChatApp/Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <SDL.h>
+#include <SDL_net.h>
+
+int main(int argc, char* argv[])
+{
+
+	std::cout << "Hey it's Chat";
+	system("pause");
+	return 0;
+}
