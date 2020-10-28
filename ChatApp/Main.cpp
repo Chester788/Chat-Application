9f791432b40d651475bc00ddc6eff649d9f56c18
Oleg <obatrov@yandex.ru>
2020-10-28 07:47:46 +0000

    Changes

--- ChatApp/Main.cpp
+++ ChatApp/Main.cpp
@@ -5,8 +5,37 @@
 
 int main(int argc, char* argv[])
 {
+	if (SDL_Init(SDL_INIT_EVERYTHING) == -1)
+	{
+		std::cout << "Error init SDL" << std::endl;
+		system("pause");
+		return 0;
 
-	std::cout << "Hey it's Chat";
+	}
+
+	if (SDLNet_Init() == -1)
+	{
+		std::cout << "Error init networking component" << std::endl;
+		system("pause");
+		return 0;
+	}
+
+	
+	std::cout << "#########################################" << std::endl;
+	std::cout << "Weclome! This is simple Chat Application" << std::endl;
+	std::cout << "#########################################" << std::endl;
+
+	std::cout << "Waiting for the chat!" << std::endl;
+
+	while (1)
+	{
+		std::cout << ".";
+		SDL_Delay(200);
+
+	}
+	
+
+
 	system("pause");
 	return 0;
 }
