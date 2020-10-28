390912bf23b75aaf2e5371aafc0feef2ae1a2f04
Oleg <obatrov@yandex.ru>
2020-10-28 13:22:13 +0000

    Changes To Communication

--- ChatApp(Client)/ClientMain.cpp
+++ ChatApp(Client)/ClientMain.cpp
@@ -9,6 +9,16 @@ const int BUFFER_SIZE = 2000;
 //Struct to store host addres and port number
 IPaddress ip; // creating Ip address
 
+//Short contstruction for chat(to send and receive messages from client and server) (For making a class later)
+bool ChatIsOpen = true;
+bool DeliverMessage = true;
+char SendMessage[BUFFER_SIZE] = { '\0' };
+//we add +1 for terminating null, we need length of message to send data
+std::string Message;
+int messageLenght = Message.length() + 1;
+
+
+
 //socket to transfer/connecting data to server
 TCPsocket Socket = nullptr;
 
@@ -63,19 +73,53 @@ int main(int argc, char* argv[])
 	//String received message
 	char message[BUFFER_SIZE];
 
-	//Receive message from server
-	if (SDLNet_TCP_Recv(Socket, message, BUFFER_SIZE) <= 0) //sending data with the client socket, data and then lenght of the message
-															// If return value is less than length of messsage there will be an error
-
+	//Receive message from server with While Loop
+	while (ChatIsOpen)
 	{
-		std::cout << "Error receiving message!" << std::endl;
-	}
+
+		if (DeliverMessage)
+		{
+
+			if (SDLNet_TCP_Recv(Socket, message, BUFFER_SIZE) <= 0) //sending data with the client socket, data and then lenght of the message
+																	// If return value is less than length of messsage there will be an error
+
+			{
+				std::cout << "Error receiving message!" << std::endl;
+			}
 
 
-	else
-	{
-		std::cout << message << std::endl;
+			else
+			{
+				std::cout << message << std::endl;
+				DeliverMessage = false;
+				system("pause");
+
+			}
+		}
+
+				else
+				{
+					std::cout << "Send your message:";
+					std::getline(std::cin, Message);
+					if (SDLNet_TCP_Send(Socket, Message.c_str(), messageLenght) < messageLenght)
+					{
+						std::cout << "Error sending message to server!" << std::endl;
+						system("pause");
+					}
+
+					else
+					{
+						
+						std::cout << "Message sent to server" << std::endl;
+						system("pause");
+						Message.clear();
+						DeliverMessage = true;
+					}
+
+				}
 	}
+	
+
 
 	//Once everything is finshed the socket has to be closed
 	SDLNet_TCP_Close(Socket);
--- ChatApp/ServerMain.cpp
+++ ChatApp/ServerMain.cpp
@@ -4,6 +4,8 @@
 #include <SDL_net.h>
 
 const int PORT = 1234;
+const int BUFFER_SIZE = 2000;
+
 //message to send to clients
 std::string message;
 
@@ -15,6 +17,13 @@ TCPsocket listenSocket = nullptr;
 
 //socket to transfer/connecting data to client
 TCPsocket clientSocket = nullptr;
+
+
+//Short contstruction for chat(to send and receive messages from client and server) (For making a class later)
+bool ChatIsOpen = true;
+bool DeliverMessage = true;
+char SendMessage[BUFFER_SIZE] = { '\0' };
+
 
 int main(int argc, char* argv[])
 {
@@ -54,7 +63,7 @@ int main(int argc, char* argv[])
 		system("pause");
 		return 0;
 	}
-
+	
 	std::cout << "Server is waiting for clients to connect!" << std::endl;
 
 	while (!clientSocket) //Breaks the lopp when the client is connecting
@@ -80,26 +89,56 @@ int main(int argc, char* argv[])
 	//we add +1 for terminating null, we need length of message to send data
 	int messageLenght = message.length() + 1;  
 
-	if (SDLNet_TCP_Send(clientSocket, message.c_str(), messageLenght) < messageLenght) //sending data with the client socket, data and then lenght of the message
+	while (ChatIsOpen)
+	{
+
+		if (DeliverMessage)
+		{
+
+			if (SDLNet_TCP_Send(clientSocket, message.c_str(), messageLenght) < messageLenght) //sending data with the client socket, data and then lenght of the message
 																						// If return value is less than length of messsage there will be an error
 
-		{
-			std::cout << "Error sending message to client!" << std::endl;
+			{
+				std::cout << "Error sending message to client!" << std::endl;
+			}
+
+
+			else
+			{
+				std::cout << message << std::endl;
+				DeliverMessage = false;
+				system("pause");
+
+			}
 		}
 
-	
+
 		else
 		{
-			std::cout << "Message sent to client!" << std::endl;
+			std::cout << "Send your message:";
+			std::getline(std::cin, message);
+			if (SDLNet_TCP_Send(clientSocket, message.c_str(), messageLenght) < messageLenght)
+			{
+				std::cout << "Error sending message to client!" << std::endl;
+				system("pause");
+			}
+
+			else
+			{
+				
+				std::cout << "Message sent to client!" << std::endl;
+				system("pause");
+				message.clear();
+				DeliverMessage = true;
+			}
+
 		}
+	}
 	
 	//Once everything is finshed the socket has to be closed
 	SDLNet_TCP_Close(clientSocket);
 
 
-
-
-		
 		//Closing tasks
 		SDLNet_Quit();
 		SDL_Quit();
